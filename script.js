a21676c3ca88eb8ea2f61556c4d8e75768bb6dd5
lord-ace <emmanuelashinze612@gmail.com>
2024-12-22 18:42:55 +0100

    christmas project

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded",
+  function(){
+    // localStorage.clear("name")
+    const main = document.querySelectorAll(".main")
+    const myName = document.querySelectorAll('.name')
+    main.forEach(element =>{
+      element.classList.remove("container")
+    })
+    const name = document.getElementById('name')
+    const savedName = localStorage.getItem("name")
+    if (savedName == null || savedName == ""){
+      main[0].classList.add("container")
+      document.getElementById("button").addEventListener('click', function(){
+        localStorage.setItem('name', name.value)
+      })
+    }
+    else{
+      main[1].classList.add("container")
+      myName.forEach(name => {
+        name.textContent = savedName
+      })
+    }
+  }
+)
